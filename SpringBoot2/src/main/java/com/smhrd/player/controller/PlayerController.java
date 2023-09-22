acc0fa7f6b470a8a017c3fc9d1766e7731d7ed12
HwangJeongyong <jeongyong62@gmail.com>
2023-09-22 14:20:55 +0900

    주소 변경

--- SpringBoot2/src/main/java/com/smhrd/player/controller/PlayerController.java
+++ SpringBoot2/src/main/java/com/smhrd/player/controller/PlayerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.smhrd.player.service.PlayerService;
 
 @RestController // 리액트 서버로 데이터만 응답
-@CrossOrigin("http://43.200.8.219:3000/") // aws : 서버주소 변경
+@CrossOrigin("http://3.37.86.114:3000/") // aws : 서버주소 변경
 public class PlayerController {
 	
 	@Autowired
